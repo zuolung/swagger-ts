1340e2f07a0d2e241880fde82b5591c0585e6844
kongjing <kongjing@dian.so>
2024-01-14 20:59:43 +0800

    feat: update

--- pages/api/mock.ts
+++ pages/api/mock.ts
@@ -7,10 +7,14 @@ export default async function (req: any, res: NextApiResponse) {
 
     res.setHeader('Content-type', 'text/plain; charset=utf-8')
 
-    const mockData = await prettier.format(req.query.data, {
+    const nData = req.query.data.replace(/\{\}/g, '"$Object@"').replace(/\[\]/g, '"$Array@"')
+
+    let mockData = await prettier.format(nData, {
       semi: false,
       parser: "json",
     });
+
+    mockData = mockData.replace(/\"\$Object\@\"/g, '{}').replace(/\"\$Array\@\"/g, '[]')
 
     res.status(200).end(mockData);
   }
